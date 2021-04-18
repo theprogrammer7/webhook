5c28b25e3a4d9bd369ff2b1cdcf0bbaf805c64f9
theprogrammer7 <72264959+theprogrammer7@users.noreply.github.com>
2021-04-18 12:56:55 +0530

    Update index.py

--- index.py
+++ index.py
@@ -3,14 +3,18 @@ from flask import Flask, request, abort
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['PUT'])
 def webhook():
-	if request.method == 'POST':
+	if request.method == 'PUT':
 		print(request.json)
+		#with open('file.txt', 'a+') as data:
+    			#data.write(request.json+'\n')
+
 		return 'success', 200
 	else:
 		abort(400)
 
 
 if __name__ == '__main__':
+	#app.run()
 	app.run(debug = False,host='0.0.0.0')
