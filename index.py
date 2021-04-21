c8b6182e32e13be49021dea7f1ec92f212287973
theprogrammer7 <72264959+theprogrammer7@users.noreply.github.com>
2021-04-21 23:26:11 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,20 +1,25 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort,render_template
+
 
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/', methods=['POST','GET'])
 def webhook():
-	if request.method == 'POST':
-		print(request.json)
-		with open('/opt/lampp/htdocs/webhook/file.txt', 'a+') as data:
-    			data.write(request.json)
+    if request.method == 'POST':
+        print(str(request.json))
+        with open('file.txt', 'a+') as data:
+                data.write(str(request.json))
 
-		return 'success', 200
-	else:
-		abort(400)
+        return 'success', 200
+
+    elif request.method == 'GET':
+            with open('wifi.txt', 'r') as f: 
+                return render_template('index.html', text=f.read())
+    else:
+        abort(400)
 
 
 if __name__ == '__main__':
-	#app.run()
-	app.run(debug = False,host='0.0.0.0',port=5000)
+    #app.run()
+    app.run(debug = False,host='0.0.0.0',port=5000)
